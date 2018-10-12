718f0fbbae2fc2f3bea176b5bcfdce163d34273d
HeatherRB <heather.reeve.black@gmail.com>
2018-10-12 21:02:22 +0100

    Added logistic regression

--- ml/linear_classifier.py
+++ ml/linear_classifier.py
@@ -8,51 +8,69 @@ class _LinearClassifier:
 
     Attributes:
         w - array of weights
+        random_state - seed for random starting weights
     """
 
-    def __init__(self):
+    def __init__(self, random_state):
 
         # initiate the array of weights
         self.w = None
+        self.random_state = random_state
 
     def _net_input(self, x):
-        """ Function to calculate the net input z = x.w
+        """ Net input
 
         Input:
             x - array of training data (without first column of 1s)
         Returns:
-            z = x.w[1:] + w[0]
+            net input z = x.w
         """
 
         n_samples = x.shape[0]
         x_ext = np.concatenate((np.ones((n_samples, 1)), x), axis=1)
 
-        return np.dot(x_ext, self.w)
+        return x_ext.dot(self.w)
 
-    def _activation(self, x):
+    def _activation(self, z):
         """ Default activation function (perceptron)
         Inputs:
-            a - array
+            z - scalar/array
         Outputs:
-            phi(a) - value of perceptron activation function
+            value of perceptron activation function
+            phi(z) =    +1  if z >= 0
+                        -1  otherwise
         """
 
-        return np.where(x >= 0, 1, -1)
+        return np.where(z >= 0, 1, -1)
 
-    def predict(self, x):
+    def _cost(self, x, y):
+        """ Default cost function (perceptron) encapsulating the error in the model
+
+        Inputs:
+            x - training data (without first column of 1s)
+            y - classification data
+        Returns:
+            value of the cost function, i.e., the number of errors in prediction
+        """
+
+        return (y - self._activation(self._net_input(x)) != 0).sum()
+
+    def predict(self, x, a=0):
         """ Function to predict the class of each sample (row) in x
 
         Inputs:
             x - training data (without first column of 1s)
+            a (optional) - threshold value above which prediction is positive
         Returns:
             array of predicted classifications (values +1/-1)
 
-        For step function, predicted class of each sample v is:
-            +1  if v.w >= 0
-            -1  otherwise
+        Predicted class of each sample is:
+            +1  if phi(z) >= a
+            -1  otherwise,
+            where z = x.w
         """
 
-        return np.where(self._activation(self._net_input(x)) >= 0, 1, -1)
+        return np.where(self._activation(self._net_input(x)) >= a, 1, -1)
 
     def _delta_w(self, x, y):
         """ Function to calculate the (unscaled) change to the weights array
@@ -67,30 +85,34 @@ class _LinearClassifier:
         n_samples = x.shape[0]
         x_ext = np.concatenate((np.ones((n_samples, 1)), x), axis=1)
 
-        return np.dot(y - self._activation(self._net_input(x)), x_ext)
+        return (y - self._activation(self._net_input(x))).dot(x_ext)
 
-
-class Perceptron(_LinearClassifier):
-    """ Perceptron classifier
-
-    Attributes:
-        w - array of weights
-    """
-
-    def __init__(self):
-        _LinearClassifier.__init__(self)
-
-    def _cost(self, x, y):
-        """ The cost function encapsulating the error in the model
+    def fit_stochastic(self, x, y, n_iterations, learning_rate):
+        """ Method to fit the model to given set of training data
 
         Inputs:
-            x - training data (without first column of 1s)
-            y - classification data
+            x - array of training data (without first column of 1s)
+            y - array of classifications for training data
+            n_iterations - number of iterations of the algorithm to perform
+            learning_rate - scale factor applied to updates of the weights array
         Returns:
-            value of the cost function, i.e., the number of errors in prediction
+            array of values of the cost function at each iteration
         """
 
-        return (y - self._activation(self._net_input(x)) != 0).sum()
+        n_samples = x.shape[0]
+        n_features = x.shape[1]
+        cost_per_iteration = []
+
+        rgen = np.random.RandomState(self.random_state)
+        self.w = rgen.normal(loc=0.0, scale=0.01, size=n_features + 1)
+
+        for n in range(n_iterations):
+            cost_per_iteration.append(self._cost(x, y))
+            x, y = shuffle_training_examples(x, y)
+            for i in range(n_samples):
+                self.w += learning_rate * self._delta_w(x[i:i+1, :], y[i:i+1])
+
+        return cost_per_iteration
 
     def fit(self, x, y, n_iterations, learning_rate):
         """ Method to fit the model to given set of training data
@@ -104,18 +126,30 @@ class Perceptron(_LinearClassifier):
             array of values of the cost function at each iteration
         """
 
-        n_samples = x.shape[0]
         n_features = x.shape[1]
         cost_per_iteration = []
 
         self.w = np.random.normal(loc=0.0, scale=0.01, size=n_features + 1)
 
-        for n in range(n_iterations):
+        for _ in range(n_iterations):
             cost_per_iteration.append(self._cost(x, y))
-            for i in range(n_samples):
-                self.w += learning_rate * self._delta_w(x[i:i+1, :], y[i:i+1])
+            self.w += learning_rate * self._delta_w(x, y)
 
         return cost_per_iteration
+
+
+class Perceptron(_LinearClassifier):
+    """ Perceptron classifier
+
+    This classifier simply adopts the default activation and cost functions from the base class
+
+    Attributes:
+        w - array of weights
+        random_state - seed for random starting weights
+    """
+
+    def __init__(self, random_state):
+        _LinearClassifier.__init__(self, random_state)
 
 
 class PerceptronAdaline(_LinearClassifier):
@@ -123,21 +157,22 @@ class PerceptronAdaline(_LinearClassifier):
 
     Attributes:
         w - array of weights
-        cost_per_iteration - array of the value of the cost function at each iteration
+        random_state - seed for random starting weights
     """
 
-    def __init__(self):
-        _LinearClassifier.__init__(self)
+    def __init__(self, random_state):
+        _LinearClassifier.__init__(self, random_state)
 
-    def _activation(self, x):
+    def _activation(self, z):
         """ Activation function
         Inputs:
-            a - array
+            z - array
         Outputs:
-            a - activation function for Adaline is identity function
+            activation function for Adaline perceptron
+            phi(z) = z
         """
 
-        return x
+        return z
 
     def _cost(self, x, y):
         """ The cost function encapsulating the error in the model
@@ -146,39 +181,80 @@ class PerceptronAdaline(_LinearClassifier):
             x - training data (without first column of 1s)
             y - classification data
         Returns:
-            value of the cost function J(w), i.e., sum of squared errors
+            value of the cost function, i.e., sum of squared errors
+            J(w) = 1/2 (y - phi(z)).(y - phi(z))
         """
 
-        return 0.5 * np.dot(y - self._net_input(x), y - self._net_input(x))
+        return 0.5 * (y - self._net_input(x)).dot(y - self._net_input(x))
 
-    def fit(self, x, y, n_iterations, learning_rate, mode):
-        """ Method to fit the model to given set of training data
+
+class LogisticRegression(_LinearClassifier):
+    """ Logistic regression classifier
+
+    Attributes:
+        w - array of weights
+        random_state - seed for random starting weights
+        c - regularisation parameter
+    """
+
+    def __init__(self, random_state, c):
+        _LinearClassifier.__init__(self, random_state)
+        self.c = c
+
+    def _activation(self, z):
+        """ Activation function for logistic regression
 
         Inputs:
-            x - array of training data (without first column of 1s)
-            y - array of classifications for training data
-            n_iterations - number of iterations of the algorithm to perform
-            learning_rate - scale factor applied to updates of the weights array
-            mode - string, either "batch" or "stochastic"
+            z - array
         Returns:
-            array of values of the cost function at each iteration
+            phi(x) = 1/(1 + e^{-z})
+            """
+
+        return 1 / (1 + np.exp(-z))
+
+    def _cost(self, x, y):
+        """ Cost function for logistic regression
+
+        Inputs:
+            x - training data (without first column of 1s)
+            y - classification data
+        Returns:
+            value of the regularised cost function, i.e., (negative) log-likelihood function
+            J(w) = - y.log(phi(z)) - (1 - y).log(1 - phi(z)) + w.w
+            """
+
+        phi_z = self._activation(self._net_input(x))
+
+        return -y.dot(np.log(phi_z)) - (1 - y).dot(np.log(1 - phi_z)) + 0.5 * self.c * self.w.dot(self.w)
+
+    def predict(self, x, a=0.5):
+        """ Function to predict the class of each sample (row) in x
+
+        Inputs:
+            x - training data (without first column of 1s)
+            a (optional) - threshold value above which prediction is positive
+        Returns:
+            array of predicted classifications (values +1/-1)
+
+        Predicted class of each sample is:
+            +1  if phi(z) >= a
+            0   otherwise,
+            where z = x.w
+        """
+
+        return np.where(self._activation(self._net_input(x)) >= a, 1, 0)
+
+    def _delta_w(self, x, y):
+        """ Function to calculate the (unscaled) change to the weights array
+
+        Inputs:
+            x - training data (without first column of 1s)
+            y - classification data
+        Returns:
+            array of weight adjustments for the next iteration
         """
 
         n_samples = x.shape[0]
-        n_features = x.shape[1]
-        cost_per_iteration = []
+        x_ext = np.concatenate((np.ones((n_samples, 1)), x), axis=1)
 
-        self.w = np.random.normal(loc=0.0, scale=0.01, size=n_features + 1)
-
-        if mode == "batch":
-            for _ in range(n_iterations):
-                cost_per_iteration.append(self._cost(x, y))
-                self.w += learning_rate * self._delta_w(x, y)
-        elif mode == "stochastic":
-            for n in range(n_iterations):
-                x, y = shuffle_training_examples(x, y)
-                cost_per_iteration.append(self._cost(x, y))
-                for i in range(n_samples):
-                    self.w += learning_rate * self._delta_w(x[i:i+1, :], y[i:i+1])
-
-        return cost_per_iteration
+        return (y - self._activation(self._net_input(x))).dot(x_ext) - self.c * self.w
